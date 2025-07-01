5ba09ed7f883a30ff8295379b2e1489197d3743c
Pushpender Rajput <pushpenderrajputsp@gmail.com>
2025-07-01 09:08:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/tasks/columns.tsx
+++ src/components/tasks/columns.tsx
@@ -4,7 +4,7 @@ import { ColumnDef } from '@tanstack/react-table';
 import { Task } from '@/types';
 import { Badge } from '@/components/ui/badge';
 import { Checkbox } from '@/components/ui/checkbox';
-import { DataTableColumnHeader } from './data-table-column-header';
+import { DataTableColumnHeader } from './data-table';
 import { DataTableRowActions } from './data-table-row-actions';
 import {
   Circle,
